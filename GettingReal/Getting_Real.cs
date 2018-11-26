3313de7647baae0b1613dd7d52e961faaa02520c
Katrine Andersen <katr5256@edu.eal.dk>
2018-11-26 12:57:11 +0100

    Add project files.

---
+++ GettingReal/Getting_Real.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vælg punkt mellem 1-4");
+            int switchCase = Convert.ToInt32(Console.ReadLine());
+            switch (switchCase)
+            {
+                case 1:
+                    Console.WriteLine("1. Registrér timer");
+                     
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("2. Se afsluttede vagter for denne måned");
+
+                    break;
+
+                case 3:
+                    Console.WriteLine("3. Administration");
+
+                    break;
+
+                case 4:
+                    Console.WriteLine("4. Luk ned");
+            }
+        }
+    }
+}
